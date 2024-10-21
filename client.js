ded6f3e0a6f490c4951edfe9c050e5efc9bbdaeb
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-10-22 03:09:13 +0900

    초기화 및 서버 연결

---
+++ client.js
@@ -0,0 +1,23 @@
+import net from 'net';
+
+const HOST = 'localhost';
+const PORT = 5555;
+
+// 소켓 생성
+const client = new net.Socket();
+
+client.connect(PORT, HOST, () => {
+    console.log(`Connected to server`);
+});
+
+client.on('data', (data) => {
+    console.log(data);
+});
+
+client.on('close', () => {
+    console.log('Connection closed');
+});
+
+client.on('error', (err) => {
+    console.error('Client error:', err);
+});
---
+++ server.js
@@ -0,0 +1,25 @@
+import net from 'net';
+
+const PORT = 5555;
+
+// 서버 생성
+const server = net.createServer((socket) => {
+    console.log(`Client connected: ${socket.remoteAddress}:${socket.remotePort}`);
+
+    socket.on('data', (data) => {
+        console.log(data);
+    });
+
+    socket.on('end', () => {
+        console.log(`Client disconnected: ${socket.remoteAddress}:${socket.remotePort}`);
+    });
+
+    socket.on('error', (err) => {
+        console.log(`Socket error, ${err}`);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Echo Server listening on port ${PORT}`);
+    console.log(server.address());
+});
